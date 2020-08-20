6499a2b814a446169d28e5847f5cb81fcecbe200
Ricardo Fernando <ricardo347@msn.com>
2020-08-19 21:28:44 -0300

    commit inical

---
+++ src/test/java/br/com/zerosystems/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zerosystems.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
